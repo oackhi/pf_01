206e39aa7e891eff3a517ebbd6ccc02f278eb85f
chika <>
2023-11-14 00:00:53 +0900

    20231113-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,25 @@ $('.p-pickup__slider').slick({
     arrows: false,
     variableWidth: true
 });
+
+var mql = window.matchMedia('screen and (max-width: 768px)');
+function checkBreakPoint(mql) {
+    if (mql.matches) {
+        // スマホ向け（768px以下のとき）
+        $('.l-allitems__list').not('.slick-initialized').slick({
+            //スライドさせる
+            lidesToShow: 2,
+            arrows: false,
+            variableWidth: true
+        });
+    } else {
+        // PC向け
+        $('.l-allitems__list.slick-initialized').slick('unslick');
+    }
+}
+
+// ブレイクポイントの瞬間に発火
+mql.addListener(checkBreakPoint);
+
+// 初回チェック
+checkBreakPoint(mql);
