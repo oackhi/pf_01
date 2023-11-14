b1f2e4bf44bd6a69fe7857604823cc30691e5e73
chika <>
2023-11-14 23:43:09 +0900

    20231114-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
+// pickup
 $('.p-pickup__slider').slick({
     arrows: false,
     variableWidth: true
 });
 
-var mql = window.matchMedia('screen and (max-width: 768px)');
+// allitem, magazine
+const mql = window.matchMedia('screen and (max-width: 768px)');
+
 function checkBreakPoint(mql) {
     if (mql.matches) {
         // スマホ向け（768px以下のとき）
@@ -13,10 +16,18 @@ function checkBreakPoint(mql) {
             arrows: false,
             variableWidth: true
         });
+        $('.p-magazine__contents').not('.slick-initialized').slick({
+            //スライドさせる
+            lidesToShow: 2,
+            arrows: false,
+            variableWidth: true
+        });
     } else {
         // PC向け
         $('.l-allitems__list.slick-initialized').slick('unslick');
+        $('.p-magazine__contents.slick-initialized').slick('unslick');
     }
+
 }
 
 // ブレイクポイントの瞬間に発火
